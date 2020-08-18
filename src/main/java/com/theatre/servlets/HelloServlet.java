6c085357fca39fcf546eeabaffbc3293ccf112fe
TonyNgeno <tonyngeno123@gmail.com>
2020-08-18 12:06:37 +0300

    added homepage

--- src/main/java/com/theatre/servlets/HelloServlet.java
+++ src/main/java/com/theatre/servlets/HelloServlet.java
@@ -1,6 +1,9 @@
 package com.theatre.servlets;
 
+import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,13 +12,36 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/tony")
-public class HelloServlet extends HttpServlet {
+public class HelloServlet extends HttpServlet{
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+/*    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+        PrintWriter printWriter = servletResponse.getWriter();
+
+        printWriter.println("This is generated using generic servlet");
+
+    }*/
+
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        String name= request.getParameter("name");
+        String age= request.getParameter("age");
+        String city= request.getParameter("city");
+        
+        Integer ageInt = Integer.valueOf(age);
+        StringBuilder msg = new StringBuilder();
+
+        msg.append("Hi" +name + ",from," +city + "." );
+
+        if (ageInt >20)
+            msg.append("You should have girlfriend");
+        else
+            msg.append("No one allowed");
 
         PrintWriter printWriter = response.getWriter();
-
-        printWriter.println("This is my first servlet");
+        printWriter.println(msg);
 
     }
 
@@ -25,6 +51,7 @@ public class HelloServlet extends HttpServlet {
 
         printWriter.println("This is my first servlet");
     }
+
 
 
 }
